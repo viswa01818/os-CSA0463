f107fceecb3dffd8763563adf02e556466d4fcdf
viswa01818 <113327835+viswa01818@users.noreply.github.com>
2022-09-12 13:07:49 +0530

    Add files via upload

---
+++ program 4 in os.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+
+int main() 
+{ 
+
+  
+
+    // make two process which run same 
+
+    // program after this instruction 
+
+
+  
+
+    printf("Hello world!\n"); 
+
+    return 0; 
+}
